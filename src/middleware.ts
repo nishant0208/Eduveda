f4959e40e585e73ed3d0b7877d98e7320d8e02a5
Nishant Tiwary <nishantkumartiwary2@gmail.com>
2025-05-21 15:26:53 +0530

    middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,24 +9,11 @@ const matchers = Object.keys(routeAccessMap).map((route) => ({
 
 console.log(matchers);
 
-export default clerkMiddleware(async(auth, req) => {
-  // if (isProtectedRoute(req)) auth().protect()
- const authObject = await auth();
-  const { sessionClaims } = authObject;
-
-  const role = (sessionClaims?.metadata as { role?: string })?.role;
-
-  for (const { matcher, allowedRoles } of matchers) {
-    if (matcher(req) && !allowedRoles.includes(role!)) {
-      return NextResponse.redirect(new URL(`/${role}`, req.url));
-    }
-  }
-});
-
+export default clerkMiddleware();
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/((?!_next|[^?]\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).)",
     // Always run for API routes
     "/(api|trpc)(.*)",
   ],
