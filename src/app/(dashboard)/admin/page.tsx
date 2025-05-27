73c134513013db5c5c6cbbe1761c3a3fdb13ee79
dipenbhat557 <pandeypawan2061@gmail.com>
2025-05-27 21:59:27 +0530

    feat: build fix

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,47 +1,84 @@
-import Announcements from "@/components/Announcements"
-import AttendanceChart from "@/components/AttendanceChart"
-import AttendanceChartContainer from "@/components/AttendanceChartContainer"
-import CountChart from "@/components/CountChart"
-import CountChartContainer from "@/components/CountChartContainer"
-import Eventcalendar from "@/components/EventCalendar"
-import FinanceChart from "@/components/FinanceChart"
-import UserCard from "@/components/UserCard"
+import dynamic from "next/dynamic";
+
+const Announcements = dynamic(() => import("@/components/Announcements"), {
+  ssr: false,
+  loading: () => <div className="animate-pulse bg-gray-200 h-32 rounded"></div>,
+});
+
+const AttendanceChartContainer = dynamic(
+  () => import("@/components/AttendanceChartContainer"),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="animate-pulse bg-gray-200 h-[450px] rounded"></div>
+    ),
+  }
+);
+
+const CountChartContainer = dynamic(
+  () => import("@/components/CountChartContainer"),
+  {
+    ssr: false,
+    loading: () => (
+      <div className="animate-pulse bg-gray-200 h-[450px] rounded"></div>
+    ),
+  }
+);
+
+const Eventcalendar = dynamic(() => import("@/components/EventCalendar"), {
+  ssr: false,
+  loading: () => <div className="animate-pulse bg-gray-200 h-64 rounded"></div>,
+});
+
+const FinanceChart = dynamic(() => import("@/components/FinanceChart"), {
+  ssr: false,
+  loading: () => (
+    <div className="animate-pulse bg-gray-200 h-[500px] rounded"></div>
+  ),
+});
+
+const UserCard = dynamic(() => import("@/components/UserCard"), {
+  ssr: false,
+  loading: () => <div className="animate-pulse bg-gray-200 h-24 rounded"></div>,
+});
 
 const AdminPage = () => {
   return (
-    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
-      <div className='w-full lg:w-2/3 flex flex-col gap-8'>
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* USER CARDS */}
-        <div className='flex gap-4 justify-between'>
+        <div className="flex gap-4 justify-between">
           <UserCard type="student" />
           <UserCard type="teacher" />
           <UserCard type="parent" />
-         {/* <UserCard type="staff" /> */}
         </div>
-          {/*middle chart*/}
+
+        {/*middle chart*/}
         <div className="flex gap-4 flex-col lg:flex-row">
           {/*CountChart*/}
           <div className="w-full lg:w-1/3 h-[450px]">
-          <CountChartContainer/>
+            <CountChartContainer />
           </div>
           {/*Attendance chart*/}
           <div className="w-full lg:w-2/3 h-[450px]">
-          <AttendanceChartContainer/>
+            <AttendanceChartContainer />
           </div>
         </div>
+
         {/*bottom chart*/}
         <div className="w-full h-[500px]">
-          <FinanceChart/>
+          <FinanceChart />
         </div>
       </div>
+
       {/* RIGHT */}
-      <div className='w-full lg:w-1/3 flex flex-col gap-8'>
-      <Eventcalendar/>
-      <Announcements/>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <Eventcalendar />
+        <Announcements />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
